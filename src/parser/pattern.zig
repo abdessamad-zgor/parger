46d73b4a142b98c2d81a8ede79d3bdfcbb4f545a
Abdessamad Zgor <abdessamadzgor@gmail.com>
2025-03-08 01:00:17 +0000

    almost done, need to solve bug in Tokenizer.tokenize

--- src/parser/pattern.zig
+++ src/parser/pattern.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const expect = std.testing.expect;
 
 const lexemem = @import("./lexeme.zig");
@@ -7,6 +8,7 @@ const tokenm = @import("./token.zig");
 const TokenType = tokenm.TokenType;
 const Token = tokenm.Token;
 const LexemeType = lexemem.LexemeType;
+const Lexeme = lexemem.Lexeme;
 
 pub const QuantifierType = enum {
     any,
@@ -20,13 +22,12 @@ pub const Quantifier = union(QuantifierType) {
     number: usize,
 };
 
-pub const StateType = enum { Init, Intrem, Final, PostFinal };
+pub const StateType = enum { Init, Intrem, Final };
 
 pub const PatternState = union(StateType) {
     Init: void,
     Intrem: usize,
     Final: usize,
-    PostFinal: void,
 };
 
 pub const Pattern = struct {
@@ -35,70 +36,90 @@ pub const Pattern = struct {
     quantifier: Quantifier,
     state: PatternState,
     ttype: TokenType,
+    stack: ?std.ArrayList(u8),
     start: usize,
     end: usize,
 
     // for this use case, tokens comprizing of one single type of lexeme are acceptable but subsequent implementation
     // need to replace lexeme with regular expression
-    pub fn init(ttype: TokenType, lexeme: LexemeType, quantifier: Quantifier) Self {
-        return Pattern{ .lexeme = lexeme, .quantifier = quantifier, .ttype = ttype, .start = 0, .end = 0, .state = .Init };
+    pub fn init(allocator: ?Allocator, ttype: TokenType, lexeme: LexemeType, quantifier: Quantifier) Self {
+        var stack: ?std.ArrayList(u8) = null;
+        if (allocator) |_allocator| {
+            stack = std.ArrayList(u8).init(_allocator);
+        }
+        return Pattern{ .stack = stack, .lexeme = lexeme, .quantifier = quantifier, .ttype = ttype, .start = 0, .end = 0, .state = .Init };
     }
 
     pub fn reset(self: *Self) PatternState {
         self.start = 0;
         self.end = 0;
+        if (self.stack != null) {
+            self.stack.?.shrinkAndFree(0);
+        }
         return .Init;
     }
 
-    pub fn transition(self: *Self, lexeme: LexemeType, index: usize) void {
+    pub fn transition(self: *Self, lexeme: Lexeme, index: usize) !?Token {
+        if (self.stack != null) {
+            try self.stack.?.append(lexeme.to_char());
+        }
         self.state = if (lexeme == self.lexeme) switch (self.state) {
             .Init => blk: {
                 self.start = index;
                 break :blk switch (self.quantifier) {
-                    .any => PatternState{ .Intrem = 1 },
-                    .one => iblk: {
+                    .one, .any => iblk: {
                         self.end = self.start + 1;
                         break :iblk PatternState{ .Final = 1 };
                     },
                     .number => PatternState{ .Intrem = 1 },
                 };
             },
-            .Final => self.reset(),
+            .Final => |iq| blk: {
+                break :blk switch (self.quantifier) {
+                    .any => iblk: {
+                        self.end = self.end + 1;
+                        break :iblk PatternState{ .Final = iq + 1 };
+                    },
+                    else => self.reset(),
+                };
+            },
             .Intrem => |iq| switch (self.quantifier) {
-                .any => PatternState{ .Intrem = iq + 1 },
+                .any => iblk: {
+                    self.end = self.end + 1;
+                    break :iblk PatternState{ .Final = iq + 1 };
+                },
                 .one => self.reset(),
                 .number => |number| blk: {
                     if (iq == number) {
-                        self.end = iq;
+                        self.end = self.start + iq;
                         break :blk PatternState{ .Final = number };
                     } else {
                         break :blk PatternState{ .Intrem = iq + 1 };
                     }
                 },
             },
-            else => .Init,
-        } else switch (self.state) {
-            .Intrem => switch (self.quantifier) {
-                .any => blk: {
-                    self.end = @intCast(index - 1);
-                    break :blk .PostFinal;
-                },
-                else => self.reset(),
-            },
-            else => self.reset(),
-        };
+        } else self.reset();
+
+        if (self.ttype == .Word) {
+            std.debug.print("step {}:\n\tstate = {}, stack = {s}\n", .{ index, self.state, self.stack.?.items });
+        }
+
+        if (self.state == .Final) {
+            return if (self.stack != null) Token{ .ttype = self.ttype, .value = self.stack.?.items, .start = self.start, .end = self.end } else Token{ .ttype = self.ttype, .value = null, .start = self.start, .end = self.end };
+        }
+
+        return null;
     }
 };
 
 test "Pattern test" {
-    var word_pattern = Pattern.init(.Word, .literal, .any);
-    const lexemes: [6]LexemeType = .{ .literal, .literal, .literal, .literal, .literal, .dash };
+    var word_pattern = Pattern.init(std.heap.page_allocator, .Word, .literal, .any);
+    const lexemes: [6]Lexeme = .{ .{ .literal = 'z' }, .{ .literal = 'i' }, .{ .literal = 'i' }, .{ .literal = 'i' }, .{ .literal = 'g' }, .dash };
     for (lexemes, 0..) |lexeme, i| {
-        word_pattern.transition(lexeme, i);
-        if (i == 5) {
-            try expect(word_pattern.start == 0);
-            try expect(word_pattern.end == 4);
-            std.debug.print("start: {}, end: {} \n", .{ word_pattern.start, word_pattern.end });
-        }
+        _ = try word_pattern.transition(lexeme, i);
+        //try expect(word_pattern.start == 0);
+        //try expect(word_pattern.end == 4);
+        //try expect(std.mem.eql(u8, @as([]const u8, token.?.value.?), "ziiig"));
+        //std.debug.print("start: {}, end: {} \n", .{ word_pattern.start, word_pattern.end });
     }
 }
